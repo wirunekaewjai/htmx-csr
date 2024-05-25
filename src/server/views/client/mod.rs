bf6df9c70ca605d4cb576732ea12b531303d89af
Wirune Kaewjai <wirune.kae@hotmail.com>
2024-05-25 19:01:39 +0700

    cleanup

--- src/server/views/client/mod.rs
+++ src/server/views/client/mod.rs
@@ -2,7 +2,6 @@ mod album_item;
 mod album_list;
 mod err;
 mod post_content;
-mod post_item;
 mod post_list;
 mod title;
 
@@ -10,6 +9,5 @@ pub use album_item::*;
 pub use album_list::*;
 pub use err::*;
 pub use post_content::*;
-pub use post_item::*;
 pub use post_list::*;
 pub use title::*;
